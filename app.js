5231c4300700b032f5223ccf76295d062ccfcf8e
SarahGeraldo <sah.nicole16@gmail.com>
2025-03-14 12:24:14 -0300

    novas

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-// CRIACAO DE UMA BIBLIOTECA
-
 const express = require("express");
+
+// porta TCP do servidor HTTP da aplicação (app.js):
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello hello olleh");
+});
+// req = dados de requisição do servidor para o cliente
+// res = dados de resposta do cliente
+
+// arrow function:
+// "app.listen()" precisa ser o último comando executado da aplicação (app.js)
+app.listen(PORT, () => {
+  console.log(`servidor sendo executado na porta ${PORT}!!!!!!!`);
+});
