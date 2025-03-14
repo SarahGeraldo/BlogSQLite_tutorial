89067c174fb6a9e1833a4f86ad913446388c7e10
SarahGeraldo <sah.nicole16@gmail.com>
2025-03-14 12:32:07 -0300

    app

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const PORT = 3000;
 
 const app = express();
 
+// o método "express.get" necessita de dois parâmetros
+
 app.get("/", (req, res) => {
   res.send("Hello hello olleh");
 });
-// req = dados de requisição do servidor para o cliente
-// res = dados de resposta do cliente
+/* req = dados de requisição do servidor para o cliente (REQUISITION)
+ res = dados de resposta para o cliente (RESULT) */
 
 // arrow function:
 // "app.listen()" precisa ser o último comando executado da aplicação (app.js)
